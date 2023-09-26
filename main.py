4d9275b7e8cec98b0d270f94a69e2a3922340a4b
Marchandl <Marchandl@educ.college-montreal.qc.ca>
2023-09-26 12:02:03 -0400

    zdfhm

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Count words
 
 def count_word():
-    texte = str(input('phrase:'))
+    texte = str(input('phrase:')) #ecrire la phrase
     return print(len(texte.split()))
 
 
-count_word()
+count_word() #print le def
